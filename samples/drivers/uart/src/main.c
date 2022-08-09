1768ac976b108aebcc1c461027a6c40026fa8f25
zerosensei <1054536335@qq.com>
2022-08-09 11:21:00 +0800

    samples: add uart example

--- samples/drivers/uart/src/main.c
+++ samples/drivers/uart/src/main.c
@@ -10,21 +10,12 @@
 #include <logging/log.h>
 #include <drivers/pinctrl.h>
 #include <drivers/gpio.h>
-#include <drivers/timer/system_timer.h>
 
 static const char fifo_data[] = "This is uart0 fifo test.\r\n";
 #define DATA_SIZE	(sizeof(fifo_data) - 1)
 
 const struct device *uart0 = DEVICE_GET(uart0);
-const struct device *gpioa = DEVICE_GET(gpiob);
-
-extern int sys_clock_driver_init(const struct device *dev);
-
-static void timer_handler1(struct k_timer *timer);
-static void timer_handler2(struct k_timer *timer);
-
-K_TIMER_DEFINE(tmr1, timer_handler1, NULL);
-K_TIMER_DEFINE(tmr2, timer_handler2, NULL);
+const struct device *gpiob = DEVICE_GET(gpiob);
 
 static void uart_cb_handler(const struct device *dev, void *data)
 {
@@ -54,25 +45,13 @@ static void uart_cb_handler(const struct device *dev, void *data)
 
 }
 
-static void timer_handler1(struct k_timer *timer)
-{
-    printk("tmr(%#lx): %ld ms\n\n", (uint32_t)timer, k_uptime_get_32());
-    gpio_port_toggle_bits(gpioa, BIT(15));
-}
-
-static void timer_handler2(struct k_timer *timer)
-{
-    printk("tmr status: %ld\n", k_timer_status_get(&tmr1));
-}
-
 int main(void)
 {
     wch_sys_init();
     gpio_init();
     uart_init();
-    sys_clock_driver_init(NULL);
 
-    printk("wch iot test\n");
+    printk("This is uart exapmle\n");
 
     if (!device_is_ready(uart0)) {
         LOG_ERR("uart0 device not ready");
@@ -81,20 +60,9 @@ int main(void)
         }
     }
 
-    gpio_pin_configure(gpioa, 15, GPIO_OUTPUT_LOW);
-
     uart_irq_callback_set(uart0, uart_cb_handler);
     uart_irq_rx_enable(uart0);
     uart_irq_tx_enable(uart0);
-
-    while (uart_irq_rx_ready(uart0)) {
-        ;
-    }
-
-    k_timer_start(&tmr1, K_MSEC(100), K_MSEC(100));
-    k_timer_start(&tmr2, K_MSEC(200), K_MSEC(500));
-
-    // __asm__ ("ecall");
 
     while(1);
 
