9c1bdb146864ab132902bdb13b45122dbe4b9264
zerosensei <1054536335@qq.com>
2022-08-09 11:32:28 +0800

    samples: add hello world exapmle

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,22 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <common.h>
+#include <kernel.h>
 #include <soc.h>
 #include <drivers/uart.h>
-#include <logging/log.h>
 
 int main(void)
 {       
     wch_sys_init();
     uart_init();
 
-    printf("Hello world\n");
-
-    LOG_ERR("test");
-
-    uint8_t a = 1;
-    __ASSERT_NO_MSG(a==0);
+    printk("Hello world\n");
 
     while(1) {
         ;
