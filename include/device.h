dafa62cdf6c8f06ce14d08bd949a9283b3a69bee
zerosensei <1054536335@qq.com>
2022-08-10 15:48:28 +0800

    inlcude: device: add DEVICE_DEFINE

--- include/device.h
+++ include/device.h
@@ -23,15 +23,36 @@ struct device {
     const void *api;
     struct device_state *sta;
     void *data;
+    
+#ifdef CONFIG_PM_DEVICE
+	/** Reference to the device PM resources. */
+	struct pm_device * const pm;
+#endif
 };
 
 static inline int device_is_ready(const struct device *dev)
 {
-    if(dev ==  NULL) {
+    if(dev == NULL) {
         return false;
     }
 
     return dev->sta->is_initialized && (dev->sta->init_result == 0);
 }
+
+#define DEVICE_DEFINE(dev_name, init_fn, pm_device, data_ptr, cfg_ptr, api_ptr, sta_ptr,  \
+             level, prio)  \
+        const Z_DECL_ALIGN(struct device)			\
+		DEVICE_NAME_GET(dev_name) __used			\
+	    = { \
+		.cfg = (cfg_ptr),					\
+		.api = (api_ptr),					\
+		.sta = (sta_ptr),					\
+		.data = (data_ptr),					\
+		COND_CODE_1(CONFIG_PM_DEVICE, (.pm = pm_device,), ())	\
+	};		        \
+	Z_INIT_ENTRY_DEFINE(DEVICE_NAME_GET(dev_name), init_fn,		\
+		(&DEVICE_NAME_GET(dev_name)), level, prio)
+
+#include "device_extern.h"
 
 #endif /* INCLUDE_DEVICE_H */
